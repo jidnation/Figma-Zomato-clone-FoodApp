5c9a8f126b24df67d3071c67924a2f32cfc14269
JidNation <jid1074@gmail.com>
2021-11-26 13:25:59 +0300

    done with the second screen
    the verification screen

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:zomato_foodapp_design_clone/Configurations/constant.dart';
 import 'package:zomato_foodapp_design_clone/Routes/contacts.dart';
 import 'package:zomato_foodapp_design_clone/Screens/Sign_up/sign_up_page.dart';
+import 'package:zomato_foodapp_design_clone/Screens/Verify/verify_page.dart';
 
 class Routing {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -9,19 +11,26 @@ class Routing {
         {
           return MaterialPageRoute(builder: (_) =>const SignUpPage());
         }
+        case verificationPage:
+        {
+          return MaterialPageRoute(builder: (_) =>const VerificationPage());
+        }
       default:
         {
           return MaterialPageRoute(
               builder: (_) => Scaffold(
                   appBar: AppBar(
+                    backgroundColor: kPrimaryColor,
+                    elevation: 0,
                     title: const Text('Zomato FoodStore'),
                   ),
                   body: Container(
+                    alignment: Alignment.center,
                     height: double.maxFinite,
                     width: double.maxFinite,
-                    color: const Color(0xffCB202D),
+                    decoration: const BoxDecoration(gradient: lGradient),
                     child: const Center(
-                      child: Text('ERROR 404\n Page Not FOUND!',
+                      child: Text('ERROR 404\nPage Not FOUND!',
                           style: TextStyle(
                               color: Color(0xfff4f4f2),
                               fontSize: 20,
